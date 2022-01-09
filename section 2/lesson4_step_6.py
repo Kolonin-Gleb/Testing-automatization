0e7d302af9f68fade1ecd93f53c7d50a2b7195bc
Колонин Глеб <topsecretway@yandex.ru>
2022-01-09 20:28:45 +0300

    Удален закомментированный код.
    Код удален по просьбе владельца репозитория.

--- section 2/lesson4_step_6.py
+++ section 2/lesson4_step_6.py
@@ -12,14 +12,6 @@ try:
   # говорим WebDriver искать каждый элемент в течение 5 секунд
   browser.implicitly_wait(5)
 
-  # browser.get("http://suninjuly.github.io/wait1.html")
-
-  # button = browser.find_element_by_id("verify")
-  # button.click()
-  # message = browser.find_element_by_id("verify_message")
-
-  # assert "successful" in message.text
-
   browser.get("http://suninjuly.github.io/cats.html")
 
   button = browser.find_element_by_id("button")
