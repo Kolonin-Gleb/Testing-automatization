978b802f2e914420d3c46be47f028fbc6c6c059a
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-02-10 22:06:23 +0300

    1 применение паттерна Page Object

--- section 4/final_project/test_main_page.py
+++ section 4/final_project/test_main_page.py
@@ -1,11 +1,26 @@
-# тест для основной страницы
+# # тест для основной страницы
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+# # Написаный не по Page Object
+# from time import sleep
 
-# Проверка работоспособности теста. Запускать тест из директории проекта
-# pytest -v --tb=line test_main_page.py
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     browser.get(link)
+#     login_link = browser.find_element_by_css_selector("#login_link")
+#     login_link.click()
+#     sleep(3)
+
+# # Проверка работоспособности теста. 
+# # pytest -v --tb=line test_main_page.py
+
+# Написанный по Page Object
+# from .pages.base_page import BasePage # Импорт класса, описывающего возможные действия между пользователем и страницей
+
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = BasePage(browser, link)
+#     page.open()
+
+# # Проверка работоспособности теста. 
+# # pytest -v --tb=line test_main_page.py
 
