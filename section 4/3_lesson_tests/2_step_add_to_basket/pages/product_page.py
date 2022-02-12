fb7cb863be9c647edfde9b9699356b2edd42b301
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-02-12 22:04:39 +0300

    Задача решена

--- section 4/3_lesson_tests/2_step_add_to_basket/pages/product_page.py
+++ section 4/3_lesson_tests/2_step_add_to_basket/pages/product_page.py
@@ -1,6 +1,11 @@
 # Поскольку я собираюсь тестировать всего одну страницу сайта не имеет смысла 
 # создавать базовую страницу и наследоваться от неё
 
+# Импорт искл., что будет обрабатываться
+from selenium.common.exceptions import NoAlertPresentException
+# Для решения загадки
+import math
+
 # Подключение локаторов
 from .locators import ProductPageLocators
 
@@ -22,3 +27,18 @@ class ProductPage:
         add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
         add_button.click()
 
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert # Переход в окно alert
+        x = alert.text.split(" ")[2] 
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try: # Попытка работы с 2 alert, что дает верный ответ
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    
--- section 4/3_lesson_tests/2_step_add_to_basket/test_product_page.py
+++ section 4/3_lesson_tests/2_step_add_to_basket/test_product_page.py
@@ -1,24 +1,5 @@
 # Добавить новый файл для тест-кейсов, связанных со страницей ProductPage.
 
-# Импорт искл., что будет обрабатываться
-from selenium.common.exceptions import NoAlertPresentException
-
-import math
-
-def solve_quiz_and_get_code(self):
-    alert = self.browser.switch_to.alert # Переход в окно alert
-    x = alert.text.split(" ")[2] 
-    answer = str(math.log(abs((12 * math.sin(float(x))))))
-    alert.send_keys(answer)
-    alert.accept()
-    try: # Попытка работы с 2 alert, что дает верный ответ
-        alert = self.browser.switch_to.alert
-        alert_text = alert.text
-        print(f"Your code: {alert_text}")
-        alert.accept()
-    except NoAlertPresentException:
-        print("No second alert presented")
-
 
 # Импорт страницы, на которой будут проводиться тесты
 from pages.product_page import ProductPage # Нужна ли . перед pages?
@@ -31,8 +12,8 @@ def test_guest_can_add_product_to_basket(browser):
     # Нажать на кнопку добавления товара
     page.add_to_basket()
 
-    # Далее нужно решить задачку из alert
-    solve_quiz_and_get_code()
+    page.solve_quiz_and_get_code()
+
 
 # Проверка работоспособности тестов
 # -s - чтобы увидеть вывод в консоль
