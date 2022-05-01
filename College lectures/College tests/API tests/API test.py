89236d21eb25f8c06279353c2ef01f4171a06aa4
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-05-01 18:47:45 +0300

    Исправлен 1 тест API
    Теперь фотография загружается верно.

--- College lectures/College tests/API tests/API test.py
+++ College lectures/College tests/API tests/API test.py
@@ -19,10 +19,11 @@ def get_key(email: str, passwd: str):
     headers = {'email': email, 'password': passwd}
     res = requests.get('https://petfriends1.herokuapp.com/api/key', headers=headers)
     status = res.status_code
+    result = res.text
     try:
         result = res.json()
     except:
-        json.decoder.JSONDecodeError: result = res.text
+        json.decoder.JSONDecodeError
     return status, result
 
 my_api_key = get_key('koloninGS20@st.ithub.ru','RwAWCy2AKsxWTga')
@@ -36,10 +37,11 @@ def get_pets(auth_key : str, filter: str):
 
     res = requests.get('https://petfriends1.herokuapp.com/api/pets', headers=headers, params=params)
     status = res.status_code
+    result = res.text
     try:
         result = res.json()
     except:
-        json.decoder.JSONDecodeError: result = res.text
+        json.decoder.JSONDecodeError
     return status, result
 
 # my_pets = get_pets(auth_key=my_api_key, filter="my_pets")
@@ -48,58 +50,45 @@ def get_pets(auth_key : str, filter: str):
 
 # Тест POST запроса для добавления питомца без фото
 def post_create_pet_simple(auth_key: str, name: str, animal_type: str, age: int):
-    headers = {'auth_key': auth_key} # +
-    data = {'name': name, 'animal_type': animal_type, 'age': age} # +
+    headers = {'auth_key': auth_key}
+    data = {'name': name, 'animal_type': animal_type, 'age': age}
 
     res = requests.post('https://petfriends1.herokuapp.com/api/create_pet_simple', headers=headers, data=data)
     status = res.status_code
+    result = res.text
     try:
         result = res.json()
     except:
-        json.decoder.JSONDecodeError: result = res.text
+        json.decoder.JSONDecodeError
     return status, result
 
 # post_create_pet_simple(auth_key=my_api_key, name="Gogo", animal_type="Dog", age=5)
 
 
 # Тест POST запроса для добавления питомца
-def post_create_pet(data):
-    headers = { 'Content-Type': data.content_type}
+def post_create_pet(name, animal_type, age, photo_name, auth_key):
+    
+    # Упаковка данных для отправки
+    m = MultipartEncoder(
+    fields={ 'name': name, 'animal_type': animal_type, 'age': age,
+            'pet_photo': (photo_name, open(photo_name, 'rb'), 'text/plain')}
+    )
 
-    res = requests.post('https://petfriends1.herokuapp.com/api/pets', headers=headers, data=data)
+    headers = {'auth_key': auth_key, 'Content-Type': m.content_type}
+
+    res = requests.post('https://petfriends1.herokuapp.com/api/pets', headers=headers, data=m)
     status = res.status_code
+    result = res.text
     try:
         result = res.json()
     except:
-        json.decoder.JSONDecodeError: result = res.text
+        json.decoder.JSONDecodeError
     return status, result
 
-# Сбор данных для отправки
-m_data = MultipartEncoder(
-    fields={'auth_key': my_api_key, 'name': "cute cat", 'animal_type': 'Cat', 'age': "2.52",
-            'pet_photo ': ('cat2.jpg', open('cat2.jpg', 'rb'), 'text/plain')}
-    )
 
-# post_create_pet(data=m_data) #TODO: Не работает отправка фото
+post_create_pet("cute cat", "Cat", "2.5", "cat2.jpg", my_api_key)
 
 
 # Тест POST запроса на добавление фотографии
-def post_set_photo(auth_key: str, m_data):
-    headers = {'auth_key': auth_key, 'Content-Type': m_data.content_type}
-    pet_id = 1
-
-    res = requests.post(f'https://petfriends1.herokuapp.com/api/pets/set_photo/{pet_id}', headers=headers, data=m_data)
-    status = res.status_code
-    try:
-        result = res.json()
-    except:
-        json.decoder.JSONDecodeError: result = res.text
-    return status, result
-
-# Сбор данных для отправки
-m_data = MultipartEncoder(
-    fields={'pet_photo ': ('Octopus.jpg', open('Octopus.jpg', 'rb'), 'text/plain')}
-    )
-
-post_set_photo(auth_key=my_api_key, m_data=m_data)
+# def post_set_photo(auth_key: str, m_data):
 
